ecb213fce4f61b8f835bd5d5f9ca64587e2b5ee7
Hewah Bahrami <hewbahrami@gmail.com>
2020-06-06 14:30:32 -0400

    added components and an API key

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import Titles from './components/Titles';
+import Form from './components/Form';
+import Weather from './components/Weather';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+
+
+class App extends Component {
+  state = {
+    input: ''
+  }
+  render() {
+    return (
+      <div>
+        <Titles />
+        <Form />
+        <Weather />
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/Form.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Form extends Component {
+    render() {
+        return (
+            <form>
+                <input type="text" name="city"></input>
+            </form>
+        )
+    }
+}
+
+export default Form
